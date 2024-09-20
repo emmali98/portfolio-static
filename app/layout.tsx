71d75617c3855410007d26a4f5f9349b7511b4a2
emmali98 <emma.m.li.98@gmail.com>
2024-09-20 18:16:55 -0400

    Sets up header nav and global template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import NavLinks from '@/components/nav.jsx';
+
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <header><NavLinks /></header>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-    return <h1>Enter</h1>
+    return (
+      <>
+        <h1>hello</h1>
+      </>
+    )
   }
