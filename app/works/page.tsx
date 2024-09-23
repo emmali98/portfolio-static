33a3be703e9b81f586498164884968d783eca9dc
emmali98 <emma.m.li.98@gmail.com>
2024-09-23 15:08:27 -0400

    Creates modal for each work entry
    State was "lifted up" to the works page, so only one modal can be
    open at a time. Next challenge will be creating a close button...

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,22 +1,30 @@
+'use client'
 import WorkEntry from '@/components/work-entry.jsx';
 import workData from '../../public/works-data.jsx';
+import { useState } from 'react';
 
 export default function Page() {
-  const worksList = workData.map(entry => 
-      <li key={entry.path}>
-        <WorkEntry
-          title={entry.title}
-          date={entry.date}
-          tools={entry.tools}
-          path={entry.path}
-        />
-      </li>
-    );
+  const [openWork, setOpenWork] = useState('');
 
-    return <>
-      <h1>Works</h1> 
-      <ul className="works-wrapper">
-        {worksList}
-      </ul>
-    </> 
-  }
+  const changeWork = (title) => setOpenWork(title);
+
+  const worksList = workData.map(entry => 
+    <li key={entry.title}>
+      <WorkEntry
+        title={entry.title}
+        date={entry.date}
+        tools={entry.tools}
+        path={entry.path}
+        opened={openWork == entry.title}
+        onWorkChange={() => changeWork(entry.title)} 
+      />
+    </li>
+  );
+
+  return <>
+    <h1>Works</h1> 
+    <ul className="works-wrapper">
+      {worksList}
+    </ul>
+  </> 
+}
--- components/work-entry.jsx
+++ components/work-entry.jsx
@@ -1,10 +1,11 @@
 'use client'
 
-export default function WorkEntry({ title, date, tools, path}) {
+export default function WorkEntry({ title, date, tools, path, opened, onWorkChange}) {
     return (<div className="work-entry">
         <h2>Title: {title}</h2>
         <p>Date: {date}</p>
         <p>Tools: {tools}</p>
-        <a href={path}>Link</a>
+        <button onClick={onWorkChange}>Open...</button>
+        {opened && <div >Modal</div>}
     </div>)
 }
